d71a4370799c8d7e298f5c56fee93777b43be5cf
bvasiljevic99 <66568797+bvasiljevic99@users.noreply.github.com>
2020-12-25 19:47:14 +0100

    Update pitanja.js
    Ispravljene su gramaticke greske,kao i nedostatci.

--- hw3/pitanja.js
+++ hw3/pitanja.js
@@ -1,79 +1,79 @@
 let pitanja =[
     {
-       pitanje: '1.  Najpoznatija dela Stevana Sremca "Ivkova slava" i "Zona Zamfirova", oslikavaju patrijarhalni duh i orijentalnu atmosferu u jednom od naših gradova?',
-        opc1: 'Krusevac',
-        opc2: 'Nis',
-        opc3: 'Vranje', 
-        opc4: 'Prizren',
-        odgovor:  3
+        "pitanje": '1.  Najpoznatija dela Stevana Sremca "Ivkova slava" i "Zona Zamfirova", oslikavaju patrijarhalni duh i orijentalnu atmosferu u jednom od naših gradova?',
+        "opc1": 'Krusevac',
+        "opc2": 'Nis',
+        "opc3": 'Vranje', 
+        "opc4": 'Prizren',
+        "odgovor":  3
     },
 
     {
-        pitanje: '2. Koja od navedenih pesama ne pripada baladama?',
-        opc1: 'Hasanaginica',
-        opc2: 'Lorelaj', 
-        opc3: 'Mozda spava',
-        opc4: 'Romansa Mesecnika',
-        odgovor: 3
+        "pitanje": '2. Koja od navedenih pesama ne pripada baladama?',
+        "opc1": 'Hasanaginica',
+        "opc2": 'Lorelaj', 
+        "opc3": 'Mozda spava',
+        "opc4": 'Romansa Mesecnika',
+        "odgovor": 3
     },
 
     {
-        pitanje: '3.Koji srpski knjizevnik je autor drame "cudo u sarganu?',
+        "pitanje": '3.Koji srpski knjizevnik je autor drame "cudo u sarganu"?',
 
-        odgovor:'Ljubomir Simovic'
+        "odgovor":'Ljubomir Simovic'
     },
     {
-        pitanje: '4. Ko je od navedenih autora bio satiricar?',
-        opc1: 'Vasko Popa',
-        opc2: 'Milan Rakic',
-        opc3: 'Petar Petrovic Njegos',
-        opc4: 'Radoje Domanovic',
-       odgovor:4
+        "pitanje": '4. Ko je od navedenih autora bio satiricar?',
+        "opc1": 'Vasko Popa',
+        "opc2": 'Milan Rakic',
+        "opc3": 'Petar Petrovic Njegos',
+        "opc4": 'Radoje Domanovic',
+        "odgovor":4
     },
 
     {
-        pitanje: '5. Prvi srpski nobelovac je?',
+        "pitanje": '5. Prvi srpski nobelovac je?',
         
-       odgovor:'Ivo Andric'
+        "odgovor":'Ivo Andric'
     },
 
     {
-        pitanje: '6. Koja se motivacija najčešće koristi u romantičarskim delima?',
-        opc1: 'Sudbina ili slucaj', 
-        opc2: 'psiholoska', 
-        opc3: 'drustvena', 
-        opc4: 'ljubavna',
-        odgovor:1
+        "pitanje": '6. Koja se motivacija najčešće koristi u romantičarskim delima?',
+        "opc1": 'Sudbina ili slucaj', 
+        "opc2": 'psiholoska', 
+        "opc3": 'drustvena', 
+        "opc4": 'ljubavna',
+        "odgovor":1
     },
 
     {
-        pitanje: '7. Delo "Derviš i smrt" Meše Selimovića je:',
-        opc1: 'lirska poema',
-        opc2: 'psiholoski roman',
-        opc3: 'drama',
-        opc4: 'pripovetka',
-        odgovor:2
-    }
+        "pitanje": '7. Delo "Derviš i smrt" Meše Selimovića je:',
+        "opc1": 'lirska poema',
+        "opc2": 'psiholoski roman',
+        "opc3": 'drama',
+        "opc4": 'pripovetka',
+        "odgovor":2
+    },
 	{
-        pitanje: '8.Koliko pevanja je podeljeno delo Ilijada i Odiseja',
+        "pitanje": '8.Koliko pevanja je podeljeno delo Ilijada i Odiseja',
 
-        odgovor:'24'
-    }
+        "odgovor":'24'
+    },
 	 
 	 {
-        pitanje: '9. Koje od nevedenih dela pripada drami apsurda?',
-        opc1: 'Cekajuci Godoa',
-        opc2: 'Balkanski spijun',
-        opc3: 'Kir Janja',
-        opc4: 'Pokondirena tikva',
-        odgovor:1
-    }
+        "pitanje": '9. Koje od nevedenih dela pripada drami apsurda?',
+        "opc1": 'Cekajuci Godoa',
+        "opc2": 'Balkanski spijun',
+        "opc3": 'Kir Janja',
+        "opc4": 'Pokondirena tikva',
+        "odgovor":1
+    },
 	 {
-        pitanje: '10. ,,Na kom se mostu i kad mogu sresti, svemoć i nemoć, prolazno i večno?" Ova stilska figura zove se:',
-        opc1: 'gradacija',
-        opc2: 'apostrofa',
-        opc3: 'slovenska antiteza',
-        opc4: 'retorsko pitanje',
-        odgovor:4
-    }
+        "pitanje": '10. ,,Na kom se mostu i kad mogu sresti, svemoć i nemoć, prolazno i večno?" Ova stilska figura zove se:',
+        "opc1": 'gradacija',
+        "opc2": 'apostrofa',
+        "opc3": 'slovenska antiteza',
+        "opc4": 'retorsko pitanje',
+        "odgovor":4
+    },
 ]
