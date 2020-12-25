70cc074609b12f5daa26019065f8ac774d15e669
bvasiljevic99 <66568797+bvasiljevic99@users.noreply.github.com>
2020-12-25 19:44:45 +0100

    Update kviz.js
    Zavrsna verzija js fajla.Dodato je par novih instrkucija.

--- hw3/kviz.js
+++ hw3/kviz.js
@@ -1,235 +1,141 @@
+var i=0;     //podesavanje promenljivih
+var ime = " ";
+var tacniOdgovori = 0;
+var tajmer = 20;
+var zaustavljanjeTajmera = 1;
 
+document.getElementById("quiz").style.display="none";
+document.getElementById("result").style.display="none";
 
-let start = document.querySelector("#start");
-let playerName = document.querySelector("#ime");
-//Quide section
+function start(){
+    setTimeout(function(){
+    document.getElementById("quiz").style.display="inline";
+    document.getElementById("name").style.display="none";
+    document.getElementById("start").style.display="none";
+    document.getElementById("inputAnswer").style.display="none";
+    document.getElementById("odg").style.display="none";
+    document.getElementById("timer").style.display="inline";
+    ucitaj(0);
+    zapocniVreme();
+    ime=document.getElementById('name').value;
+}, 1500);
+}
 
-let guide = document.querySelector("#guide");
-let exit = document.querySelector("#exit");
-let continueBtn = document.querySelector("#continue");
-
-//Quiz section
-
-let quiz = document.querySelector("#quiz");
-let time = document.querySelector("#time");
-
-//Question section
-let questionNo = document.querySelector("#questionNo");
-let questionText = document.querySelector("#questionText");
-
-//Answers
-let opt1 = document.querySelector("#opt1");
-let opt2 = document.querySelector("#opt2");
-let opt3 = document.querySelector("#opt3");
-let opt4 = document.querySelector("#opt4");
-let inputOdgovor = document.querySelector("#inputOdgovor");
-
-//Score and next question
-let total_correct = document.querySelector("#total_correct");
-let next_question = document.querySelector("#next_question");
-
-//Result section
-let result = document.querySelector("#result");
-let points = document.querySelector("#points");
-let quit = document.querySelector("#quit");
-let startAgain = document.querySelector("#startAgain");
-
-//Choices 
-let choice_que = document.querySelectorAll(".choice_que");
-let btnn = document.querySelector(".btnn");
-
-let i = 0;
-let index = 0;
-let timer = 0;
-let interval = 0;
-
-//Score
-let correct = 0;
-
-//BTN INPUT ANSWER
-btnn.addEventListener('click', ()=>{
-    next_question.style.display = "none";
-    let ia = inputAnswer.value.toLowerCase();
-    console.log(questions2[i-1].answer);
-    console.log(ia);
-    if(questions2[i-1].answer == ia){
-        correct++;
+function ucitaj(broj){
+    if ((i+1)!=3 && (i+1)!=5 && (i+1)!=8){
+        document.getElementById("questionText").innerHTML=pitanja[broj].pitanje;
+        document.getElementById("odgovor11").innerHTML=pitanja[broj].opc1;
+        document.getElementById("odgovor22").innerHTML=pitanja[broj].opc2;
+        document.getElementById("odgovor33").innerHTML=pitanja[broj].opc3;
+        document.getElementById("odgovor44").innerHTML=pitanja[broj].opc4;
+        document.getElementById("name").style.display="none";
+        document.getElementById("start").style.display="none";
+        document.getElementById("inputAnswer").style.display="none";
+        document.getElementById("odg").style.display="none";
+        document.getElementById("dugmad").style.display="inline";
     }
     else{
-        correct += 0;
-    }
-
-    if(index >= MCQS.length-1 && index < 9 ){
-        opc1.style.display = "none";
-        opc2.style.display = "none";
-        opc3.style.display = "none";
-        opc4.style.display = "none";
-        index++;
-
-        questionText.innerText = questions2[i].question;
-        inputOdgovor.style.display = "block";
-
-        timer = 0;
-        clearInterval(interval);
-        interval = setInterval(countDown, 1000);
-        i++;
-    }
-
-    else{
-        index = 0;
-
-        //result section
-        clearInterval(interval);
-        quiz.style.display= "none";
-        result.style.display = "block";
-        points.innerText = correct;
-    }
-})
-
-
-//Answer
-let UserAns = undefined;
-
-//SUBMIT BUTTON
-start.addEventListener('click', ()=>{
-    btnn.style.display = "none";
-    console.log(playerName.value);
-    console.log(correct);
-    start.style.display = "none";
-    playerName.style.display = "none";
-    guide.style.display = "block";  
-});
-
-//EXIT BUTTON
-exit.addEventListener('click', ()=>{
-    quiz.style.display = "none";
-    result.style.display = "block";
-});
-
-//TIMER
-let countDown = ()=>{
-    if(timer === 20){
-        clearInterval(interval);
-        next_question.click();
-    }
-    else{
-        timer++;
-        time.innerText = timer;
+        document.getElementById("questionText").innerHTML=pitanja[broj].pitanje;
+        document.getElementById("inputAnswer").style.display="inline";
+        document.getElementById("odg").style.display="inline";
+        document.getElementById("dugmad").style.display="none";
     }
 }
 
-//setInterval(countDown, 1000);
-
-let loadData = ()=>{
-    questionNo.innerText = index + 1 + ". ";
-    questionText.innerText = pitanja[index].question;
-    opt1.innerText = pitanja[index].opc1;
-    opt2.innerText = pitanja[index].opc2;
-    opt3.innerText = pitanja[index].opc3;
-    opt4.innerText = pitanja[index].opc4;
-
-    //start timer
-    timer = 0;
-}
-
-loadData();
-
-//CONTINUE BUTTON
-continueBtn.addEventListener('click', ()=>{
-    quiz.style.display = "block";
-    guide.style.display = "none";
-    interval = setInterval(countDown, 1000);
-    loadData();
-
-    choice_que.forEach(removActive=>{
-        removActive.classList.remove("active");
-    })
-});
-
-opc_que.forEach( (opc, choiceNo) => {
-   opc.addEventListener("click", ()=>{
-       opcs.classList.add("active");
-       //check answer
-       if(opcNo === pitanja[index].answer){
-           correct++;
-       }
-       else{
-           correct += 0;
-       }
-       console.log(correct);
-       //stop counter
-       clearInterval(interval);
-
-       //disable other choices when the answer is selected
-       for(let i = 0; i <=3; i++){
-           opc_que[i].classList.add("disabled");
-       }
-   }) 
-});
-
-next_question.addEventListener("click", ()=>{
-    if(index < MCQS.length-1){
-        index++;
-        opc_que.forEach(removActive=>{
-            removActive.classList.remove("active");
-        })
-
-        loadData();
-        clearInterval(interval);
-        interval = setInterval(countDown, 1000);
+function proveraDugme(){
+    if ((i+1)!=3 && (i+1)!=5 && (i+1)!=8 && i<10){
+        if (pitanja[i].odgovor==1){
+            document.getElementById("odgovor1").style.backgroundColor = "green";
+            tacniOdgovori++;
+        }
+        if (pitanja[i].odgovor==2){
+            document.getElementById("odgovor2").style.backgroundColor = "green";
+            tacniOdgovori++;
+        }
+        if (pitanja[i].odgovor==3){
+            document.getElementById("odgovor3").style.backgroundColor = "green";
+            tacniOdgovori++;
+        }
+        if (pitanja[i].odgovor==4){
+            document.getElementById("odgovor4").style.backgroundColor = "green";
+            tacniOdgovori++;
+        }
     }
-    else if(index >= pitanja.length-1 && index < 9 ){
-        btnn.style.display = "block";
-        next_question.style.display = "none";
-        opt1.style.display = "none";
-        opt2.style.display = "none";
-        opt3.style.display = "none";
-        opt4.style.display = "none";
-        index++;
-
-        questionText.innerText = questions2[i].question;
-        inputAnswer.style.display = "block";
-
-        /*timer = 0;*/
-        clearInterval(interval);
-        interval = setInterval(countDown, 1000);
+    else {
+        if(document.getElementById('inputAnswer').value.toUpperCase() == pitanja[i].odgovor.toUpperCase()){
+            alert("Odgovor je tacan!")
+            tacniOdgovori++;
+        }
+        else{
+            alert("Odgovor je netacan!")
+        }
+        }
+    zaustavljanjeTajmera=0;
+    tajmer = 20;
+    setTimeout(function(){
+        if (i>10){
+            odustani()
+        }
+        else{
+            zaustavljanjeTajmera=1;
+            document.getElementById("timer").style.display="inline";
+        }
         i++;
-    }
-
-    else{
-        index = 0;
-
-        //result section
-        clearInterval(interval);
-        quiz.style.display= "none";
-        result.style.display = "block";
-        points.innerText = correct;
-    }
-
-    for(let i = 0; i <=3; i++){
-        opc_que[i].classList.remove("disabled");
-    }
-})
-
-
-quit.addEventListener("click", ()=>{
-    result.style.display = "none";
-    correct = 0;
-    playerName.value = undefined;
-})
-
-startAgain.addEventListener("click", ()=>{
-    btnn.style.display = "none";
-    result.style.display = "none";
-    start.style.display = "block";
-    playerName.style.display = "block";
-    correct = 0;
-    index = 0;
-    i = 0;
-    next_question.style.display = "block";
-    opt1.style.display = "block";
-    opt2.style.display = "block";
-    opt3.style.display = "block";
-    opt4.style.display = "block";
-    inputOdgovor.style.display = "none";
-})
+        ucitaj(i);
+        ocisti();
+        
+       
+    },3000)
+}
+function ocisti(){
+	document.getElementById("odgovor1").style.backgroundColor = "#d0451b";
+	document.getElementById("odgovor2").style.backgroundColor = "#d0451b";
+    document.getElementById("odgovor3").style.backgroundColor = "#d0451b";
+    document.getElementById("odgovor4").style.backgroundColor = "#d0451b";
+	document.getElementById("inputAnswer").value="";
+}
+function zapocniVreme(){
+	document.getElementById('timer').innerHTML=tajmer;
+	function vreme(){
+		if (zaustavljanjeTajmera>0){
+			tajmer--;
+			document.getElementById('timer').innerHTML=tajmer;
+		}
+		else{
+			document.getElementById('timer').innerHTML=tajmer;
+		}
+		if (tajmer==0){
+			alert("Vreme za odgovor je isteklo!")
+			proveraDugme();
+}
+	}
+setInterval(vreme, 1000); //postavljanje intervala na 1 sekundu
+}
+function preskociPitanje(){
+    zaustavljanjeTajmera=0;
+    tajmer=20;
+    document.getElementById("odg").style.display="none";
+    setTimeout(function(){
+        if (i>10){
+            odustani()
+        }
+        else{
+            zaustavljanjeTajmera=1;
+            document.getElementById("timer").style.display="inline";
+        }
+        i++;
+        ucitaj(i);
+        ocisti();
+        
+       
+    },3000)
+}
+function odustani(){
+    document.getElementById("questionText").innerHTML=ime + " je osvojio/la " + tacniOdgovori + "/10!"
+    document.getElementById("inputAnswer").style.display="none";
+    document.getElementById("odg").style.display="none";
+    document.getElementById("dugmad").style.display="none";
+    document.getElementById("answersSection").style.display="none";
+    zaustavljanjeTajmera=0;
+    document.getElementById("timer").style.display="none";
+}
