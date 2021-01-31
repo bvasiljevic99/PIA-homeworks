2f4f912f65e6de27f2de54a4c04209875f59c33b
bvasiljevic99 <66568797+bvasiljevic99@users.noreply.github.com>
2021-01-31 18:50:39 +0100

    registacija na sajt

---
+++ hw-4/registrovanje.php
@@ -0,0 +1,167 @@
+<?php
+session_start();
+include ("selektovanje.php");
+
+$ime = "";
+$prezime ="";
+$email ="";
+$korisnicko_ime = "";
+$lozinka = "";
+$ime_err = "";
+$prezime_err =""; 
+$email_err = "";
+$korisnicko_ime_err = ""; 
+$lozinka_err = "";
+ 
+if($_SERVER["zahtev"] == "POST"){
+	
+
+	if(empty(trim($_POST["ime"]))){
+        $ime_err = "potrebno je uneti ime.";
+	} else{
+        $ime = trim($_POST["ime"]);
+    }
+	
+	
+	if(empty(trim($_POST["prezime"]))){
+        $prezime_err = "potrebno je uneti prezime.";
+	} else{
+        $prezime = trim($_POST["prezime"]);
+    }
+	
+	
+    if(empty(trim($_POST["email"]))){
+        $email_err = "potrebno je uneti email adresu.";
+    } else{
+        $sql = "SELECT id_registrovani FROM ucesnici WHERE email = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "s", $param_email);
+
+            $param_email = trim($_POST["email"]);
+            
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $email_err = "Ova email adresa vec postoji.";
+                } else{
+                    $email = trim($_POST["email"]);
+                }
+            } else{
+                echo "Ups!Sistem javlja gresku.Pokusaj opet.";
+            }
+			
+            mysqli_stmt_close($stmt);
+        }
+    }
+ 
+    
+    if(empty(trim($_POST["korisnicko_ime"]))){
+        $korisnicko_ime_err = " korisničko ime.";
+    } else{
+        $sql = "SELECT id_korisnika FROM ucesnici WHERE korisnicko_ime = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "s", $param_korisnicko_ime);
+
+            $param_korisnicko_ime = trim($_POST["korisnicko_ime"]);
+            
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $korisnicko_ime_err = "Ovo korisničko ime je već rezervisano.";
+                } else{
+                    $korisnicko_ime = trim($_POST["korisnicko_ime"]);
+                }
+            } else{
+                echo "Došlo je do greške! Pokušajte ponovo.";
+            }
+			
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+   
+    if(empty(trim($_POST["lozinka"]))){
+        $lozinka_err = " lozinku.";     
+ else{
+        $lozinka = trim($_POST["lozinka"]);
+    }
+    
+	
+	$param_ime = $ime;
+	$param_prezime = $prezime;
+	$param_email = $email;
+    $param_korisnicko_ime = $korisnicko_ime;
+    $param_lozinka = $lozinka;
+	
+    if(empty($ime_err) && empty($prezime_err) && empty($email_err) && empty($korisnicko_ime_err) && empty($lozinka_err)){
+        
+        $sql = "INSERT INTO ucesnici (ime, prezime, email, korisnicko_ime, lozinka) VALUES (?, ?, ?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "sssss", $param_ime, $param_prezime, $param_email, $param_korisnicko_ime, $param_lozinka);
+            
+			
+            
+            if(mysqli_stmt_execute($stmt)){
+                header("location: login.php");
+            } else{
+                echo "Došlo je do greške! Pokušajte kasnije.";
+            }
+            mysqli_stmt_close($stmt);
+        }
+    }
+    mysqli_close($link);
+}
+?>
+ 
+
+<html>
+	<head>
+		<title>Registrovanje</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+		<link href="stilizacija.css" rel="stylesheet">
+	</head>
+<body>
+    <div class="pocetna">
+        <h2>Registrovanje</h2>
+        <p>Potrebno je popuniti sve,radi registrovacije.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+			<div class="form-group <?php echo (!empty($ime_err)) ? 'has-error' : ''; ?>">
+                <label>Ime:</label>
+                <input type="text" name="ime" class="form-control" value="<?php echo $ime; ?>">
+                <span class="help-block"><b><?php echo $ime_err; ?></b></span>
+            </div> 
+			<div class="form-group <?php echo (!empty($prezime_err)) ? 'has-error' : ''; ?>">
+                <label>Prezime:</label>
+                <input type="text" name="prezime" class="form-control" value="<?php echo $prezime; ?>">
+                <span class="help-block"><b><?php echo $prezime_err; ?></b></span>
+            </div> 
+			<div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+                <label>Email adresa:</label>
+                <input type="email" name="email" class="form-control" value="<?php echo $email; ?>">
+                <span class="help-block"><b><?php echo $email_err; ?></b></span>
+            </div> 
+            <div class="form-group <?php echo (!empty($korisnicko_ime_err)) ? 'has-error' : ''; ?>">
+                <label>Korisničko ime:</label>
+                <input type="text" name="korisnicko_ime" class="form-control" value="<?php echo $korisnicko_ime; ?>">
+                <span class="help-block"><b><?php echo $korisnicko_ime_err; ?></b></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($lozinka_err)) ? 'has-error' : ''; ?>">
+                <label>Lozinka:</label>
+                <input type="password" name="lozinka" class="form-control" value="<?php echo $lozinka; ?>">
+                <span class="help-block"><b><?php echo $lozinka_err; ?></b></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-default" value="Reset">
+            </div>
+            <p>Već ste registrovani? <a href="login.php">Onda se ulogujte ovde.</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
